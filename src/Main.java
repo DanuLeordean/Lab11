25ef05873bddda12e00df3022546aff8bf91ef98
Danut Ilie Leordean <danuwenthome@gmail.com>
2022-10-15 18:37:16 +0300

    tema Lab 10

---
+++ src/Main.java
@@ -0,0 +1,109 @@
+public class Main {
+    public static void main(String[] args) {
+
+       /* System.out.println("Hello world!");
+
+
+        System.out.println("Hello");
+        System.out.println("Danut");
+
+        System.out.println(50+10);
+        System.out.println(30/2);
+        System.out.println(-5+8*6);
+        System.out.println((55+9)%9);
+        System.out.println(20+-3*5/8);
+        System.out.println(5+15/3*2-8%3);
+
+
+        int first  = 50;
+        int second = 10;
+        int result = first + second;
+        System.out.println( "rezultatul adunarii dintre " + first + " si " + second+ " este egal cu: " + result);
+
+        int a = 30;
+        int b = 2;
+        int c = a / b;
+        System.out.println("rezultatul impartirii dintre " + a + " si "+ b +" este egal cu: " + c);
+
+        int d = -5;
+        int e = 8;
+        int f = 6;
+        int rezultat = d + e * f;
+        System.out.println("rezultatul operatiei este:"+ rezultat);
+
+        int g = 55;
+        int h = 9;
+        int i = (g + h) % h;
+        System.out.println("rezultatul operatiei este:"+ i);
+
+        int j = 20;
+        int k = -3;
+        int l = 5;
+        int m = j + k * l / e;
+        System.out.println("rezultatul operatiei este:"+ m);
+
+        int n = 5;
+        int o = 15;
+        int p = 3;
+        int q = n + o / p * b - e / p;
+        System.out.println("rezultatul operatiei este:"+ q);*/
+
+
+        ala();
+        ba();
+        la();
+        por();
+        to();
+        ca();
+        laa();
+        cine();
+    }
+public static void ala () {
+    System.out.println("Hello");
+    }
+public static void ba () {
+    System.out.println("Danut");
+}
+public static void la () {
+    int first  = 50;
+    int second = 10;
+    int result = first + second;
+    System.out.println( "rezultatul adunarii dintre " + first + " si " + second+ " este egal cu: " + result);
+}
+public static void por () {
+    int a = 30;
+    int b = 2;
+    int c = a / b;
+    System.out.println("rezultatul impartirii dintre " + a + " si "+ b +" este egal cu: " + c);
+}
+public static void to () {
+    int d = -5;
+    int e = 8;
+    int f = 6;
+    int rezultat = d + e * f;
+    System.out.println("rezultatul operatiei este:"+ rezultat);
+}
+public static void ca () {
+    int g = 55;
+    int h = 9;
+    int i = (g + h) % h;
+    System.out.println("rezultatul operatiei este:"+ i);
+}
+public static void laa () {
+    int j = 20;
+    int e = 8;
+    int k = -3;
+    int l = 5;
+    int m = j + k * l / e;
+    System.out.println("rezultatul operatiei este:"+ m);
+}
+public static void cine () {
+    int n = 5;
+    int o = 15;
+    int e = 8;
+    int b = 2;
+    int p = 3;
+    int q = n + o / p * b - e / p;
+    System.out.println("rezultatul operatiei este:"+ q);
+}
+}
