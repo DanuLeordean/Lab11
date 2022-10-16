0cf1f550447691ba023c725bdc3a00ab8c9626f3
Danut Ilie Leordean <danuwenthome@gmail.com>
2022-10-16 10:54:44 +0300

    tema Lab 10

--- src/Main.java
+++ src/Main.java
@@ -139,14 +139,22 @@ public static int impartire () {
         System.out.println("   J    A A    V   V     A A   ");
         System.out.println("J  J   AAAAA    V V     AAAAA  ");
         System.out.println(" JJ   A     A    V     A     A ");*/
-
-        System.out.println("media celor 3 numere este:" + media());
+   /*        System.out.println("media celor 3 numere este:" + media());
 }
 public static int media () {
         int a = 10;
         int b = 20;
         int c = 30;
         int result = (a+b+c)/2;
-        return result;
+        return result;*/
+
+
+
+
+        System.out.println("  +'''''+ ");
+        System.out.println(" [| o o |]");
+        System.out.println("  |  ^  | ");
+        System.out.println("  | '-' | ");
+        System.out.println("  +-----+ ");
 }
 }
