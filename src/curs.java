94feea30378494e176d490c9cdf21f3afdccd62e
Danut Ilie Leordean <danuwenthome@gmail.com>
2022-10-23 15:26:39 +0300

    tema lab 11

--- src/curs.java
+++
@@ -1,9 +0,0 @@
-public class curs {
-
-
-    public static void main(String[] args) {
-        LogicalOperations aap = new LogicalOperations();
-
-        System.out.println(aap.textComparison( "FastTrack"));
-    }
-}
