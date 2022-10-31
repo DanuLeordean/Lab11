69b524c355ca4ddfa893bebb3d9a59b5184c30fa
Danut Ilie Leordean <danuwenthome@gmail.com>
2022-10-31 02:27:50 +0200

    tema lab 11

--- src/LogicalOperations.java
+++ src/LogicalOperations.java
@@ -1,10 +1,9 @@
 public class LogicalOperations {
 
     public int getGreaterNumber(int first, int second) {
-        if(first > second){
+        if (first > second) {
             return first;
-        }
-        else{
+        } else {
             return second;
         }
 
@@ -20,96 +19,194 @@ public class LogicalOperations {
             return "no condition was met";
         }
     }
+
     public String learnTrySomeMore(String textInput) {
-        if (textInput.equals("FastTrack")){
+        if (textInput.equals("FastTrack")) {
             return "Learning text comparison";
         } else {
             return "Got to try some more";
         }
 
     }
+
     public int checkBiggerNumber(int first, int second) {
-        if(first > second) {
+        if (first > second) {
             return first;
         } else {
             return second;
         }
     }
-    public String exCinci (String textInput, int numberInput){
-        if(textInput.equals ("FastTrack") && numberInput <=3) {
+
+    public String exCinci(String textInput, int numberInput) {
+        if (textInput.equals("FastTrack") && numberInput <= 3) {
             return textInput + numberInput;
-        }
-        else if (!textInput.equals("FastTrack") && numberInput >=4) {
+        } else if (!textInput.equals("FastTrack") && numberInput >= 4) {
             return numberInput + textInput;
-        }
-        else {
+        } else {
             return "no condition was met";
         }
 
     }
-    public String winterSnow (int numberInput) {
+
+    public String winterSnow(int numberInput) {
         if (numberInput > 8) {
             return "The amount of snow this winter was(cm): " + numberInput;
-        }else if (numberInput == 6) {
+        } else if (numberInput == 6) {
             return "The amount of snow this winter was(cm): " + numberInput;
-        }
-        else {
+        } else {
             return "The forecast snow is(cm): " + numberInput;
         }
     }
-    public String greaterThanNotEqual ( int numberInput) {
-        if(numberInput > 3 && numberInput !=4) {
+
+    public String greaterThanNotEqual(int numberInput) {
+        if (numberInput > 3 && numberInput != 4) {
             return "The number is greater than 3 and not equal to 4";
-        }
-        else if (numberInput == 4) {
+        } else if (numberInput == 4) {
             return "The number is equal to 4";
         }
         return "no condition was met";
     }
-    public void switchCase (int numberInput) {
+
+    public void switchCase(int numberInput) {
         switch (numberInput) {
-            case 1: System.out.println("The number is:  1 !");break;
-            case 2: System.out.println("The number is:  2 !");break;
-            case 3: System.out.println("The number is:  3 !");break;
-            case 4: System.out.println("The number is:  4 !");break;
-            case 5: System.out.println("The number is:  5 !");break;
-            case 6: System.out.println("The number is:  6 !");break;
-            case 7: System.out.println("The number is:  7 !");break;
-            case 8: System.out.println("The number is:  8 !");break;
-            case 9: System.out.println("The number is:  9 !");break;
-            default: System.out.println("The number is not between 1-9 !");
+            case 1:
+                System.out.println("The number is:  1 !");
+                break;
+            case 2:
+                System.out.println("The number is:  2 !");
+                break;
+            case 3:
+                System.out.println("The number is:  3 !");
+                break;
+            case 4:
+                System.out.println("The number is:  4 !");
+                break;
+            case 5:
+                System.out.println("The number is:  5 !");
+                break;
+            case 6:
+                System.out.println("The number is:  6 !");
+                break;
+            case 7:
+                System.out.println("The number is:  7 !");
+                break;
+            case 8:
+                System.out.println("The number is:  8 !");
+                break;
+            case 9:
+                System.out.println("The number is:  9 !");
+                break;
+            default:
+                System.out.println("The number is not between 1-9 !");
         }
     }
 
     public void isEvenNumber(int numberInput) {
-        if (numberInput % 2 ==0) {
+        if (numberInput % 2 == 0) {
             System.out.println("true");
-        }
-        else {
+        } else {
             System.out.println("false");
         }
     }
 
-    public void isEligibleToVote (int numberInput) {
+    public void isEligibleToVote(int numberInput) {
         if (numberInput >= 18) {
             System.out.println("True");
-        }
-        else {
+        } else {
             System.out.println("False");
         }
     }
 
-    public  int celMaiMareNumar (int first, int second, int third) {
+    public int celMaiMareNumar(int first, int second, int third) {
         if (first > second && first > third) {
             return first;
-        }
-        else if (second > first && second > third) {
+        } else if (second > first && second > third) {
             return second;
-        }
-        else if ( third > first && third > second) {
+        } else if (third > first && third > second) {
             return third;
         }
         return first;
     }
 
-}
+    public void printNumberTo100(int number) {
+        for (int i = number; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public float sumAndAverage() {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum = sum + i;
+        }
+        float average = sum / 100f;
+        return average;
+    }
+
+    public void numaratoareMinus100(int number) {
+        for (int i = number; i >= -100; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public void printfromFirstToLast(int first, int last) {
+        while (first <= last) {
+            System.out.println(first);
+            first++;
+        }
+    }
+
+    public void verificaBiggerNumberNumaratoareCrescatoare(int x, int y) {
+        if (x > y) {
+            while (y <= x) {
+                System.out.println(y);
+                y++;
+            }
+        } else {
+            while(x <= y) {
+                System.out.println(x);
+                x++;
+            }
+        }
+        }
+        public void numereParePanaLa100 (int number){
+        for (int i=1; i<= number; i++) {
+            if (i%2==0) {
+                System.out.println(i);
+            }
+        }
+        }
+
+    public void numereImparePanaLa100 (int number){
+        for (int i=1; i<= number; i++) {
+            if (i%2!=0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public float averageFromNumberToNumber (int first, int last) {
+        float sum = 0;
+        int count = 0;
+        while(first<=last){
+            sum=sum + first;
+            count ++;
+            first ++;
+
+        }
+        return sum/count;
+    }
+
+    public void fibonacci () {
+        int i =1, n= 20, firstTerm = 0, secondTerm = 1;
+        while ( i <= n){
+            System.out.println(firstTerm + ", ");
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm=secondTerm;
+            secondTerm = nextTerm;
+            i++;
+
+        }
+
+    }
+    }
--- src/Main2.java
+++ src/Main2.java
@@ -1,39 +1,34 @@
 public class Main2 {
 
     public static void main(String[] args) {
-
-
-       LogicalOperations op = new LogicalOperations();
+     /* LogicalOperations op = new LogicalOperations();
        Calculator calc = new Calculator();
-       int numberInput = 19;
-      /*  //   System.out.println(op.getGreaterNumber(first, second));
+      int numberInput = 19;
+      System.out.println(op.getGreaterNumber(first, second));
 
 
-      //  int first = 100;
-      //  int second = 400;
-      //  System.out.println(calc.adunare(first, second));
-     //   System.out.println(calc.scadere(first, second));
-     //   System.out.println(calc.inmultire(first, second));
-     //   System.out.println(calc.impartire(first,second));
+        int first = 100;
+        int second = 400;
+        System.out.println(calc.adunare(first, second));
+        System.out.println(calc.scadere(first, second));
+        System.out.println(calc.inmultire(first, second));
+       System.out.println(calc.impartire(first,second));
 
-      //  int biggest = op.checkBiggerNumber(first, second);
-     //   System.out.println("The biggest number is " + biggest); */
-      /*  String textInput = "FastTrack";
+        int biggest = op.checkBiggerNumber(first, second);
+        System.out.println("The biggest number is " + biggest);
+        String textInput = "FastTrack";
         int numberInput = 3;
         System.out.println(op.exCinci(textInput, numberInput));*/
-      /*  int numberInput = 10;
-        System.out.println(op.winterSnow(numberInput));*/
-
-        /* op.switchCase(numberInput);*/
-
-       /* op.isEvenNumber(numberInput);*/
-
-       /* op.isEligibleToVote(numberInput); */
-
-       /* int first = 75;
-        int second = 170;
-        int third = 49;
-        System.out.println(op.celMaiMareNumar( first, second, third));*/
+      /*  //int numberInput = 10;
+       // System.out.println(op.winterSnow(numberInput));
+        // op.switchCase(numberInput);
+        //op.isEvenNumber(numberInput);
+       // op.isEligibleToVote(numberInput);
+     //   int first = 75;
+      //  int second = 170;
+      //  int third = 49;
+      //  System.out.println(op.celMaiMareNumar( first, second, third));
+        */
 
 
     }
