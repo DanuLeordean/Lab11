099a91e4d566b80dc1636bea0aff00e8c3c89ae1
Danut Ilie Leordean <danuwenthome@gmail.com>
2022-10-31 02:33:37 +0200

    tema lab 11

---
+++ src/Main3.java
@@ -0,0 +1,25 @@
+public class Main3 {
+
+    public static void main(String[] args) {
+
+        LogicalOperations logicalop = new LogicalOperations();
+
+        //logicalop.printNumberTo100(1);
+       // System.out.println(logicalop.sumAndAverage());
+       // logicalop.numaratoareMinus100(1);
+       // int first = 30;
+       // int last = 50;
+       // logicalop.printfromFirstToLast(first,last);
+
+       // int x = 30;
+       // int y = 50;
+       // logicalop.verificaBiggerNumberNumaratoareCrescatoare(x, y);
+
+      //  int number=100;
+       // logicalop.numereParePanaLa100(number);
+      //  logicalop.numereImparePanaLa100(number);
+      // System.out.println(logicalop.averageFromNumberToNumber(111, 8899));
+       // logicalop.fibonacci();
+
+    }
+}
